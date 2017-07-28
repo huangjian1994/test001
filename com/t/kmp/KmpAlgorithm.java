3904b2a84011fc4415f426ebe01126264feb201a
hj <173247429@qq.comgit>
2017-07-28 17:27:33 +0800

    完善

---
+++ com/t/kmp/KmpAlgorithm.java
@@ -0,0 +1,18 @@
+package t.kmp;
+
+
+/**
+ * Author:hj 
+ * Date:  2017-7-28 
+ * Description:
+*/
+public class KmpAlgorithm {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
--- src/test.java
+++
@@ -1,7 +0,0 @@
-public class test {
-	public static void main(String[] args) {
-		long x = 2;
-		System.out.println((x++) / 3);
-	}
-
-}
